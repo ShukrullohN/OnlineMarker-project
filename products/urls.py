620b831fab645446e30708258aa54e0547317440
Shukrullo <shukrullo07@gmail.com>
2024-06-13 17:19:51 +0500

    'qwe'

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from products.views import ProductsListView
+from products.views import ProductsListView, ProductDetailView
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='list')
+    path('', ProductsListView.as_view(), name='list'),
+    path('detail/<int:pk>/', ProductDetailView.as_view(), name='detail'),
 ]
