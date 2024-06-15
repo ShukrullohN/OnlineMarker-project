e4aef168fbfe0733d1d32fc6f8a365c140ebba11
Shukrullo <shukrullo07@gmail.com>
2024-06-15 18:56:06 +0500

    'comment'

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from products.views import ProductsListView, ProductDetailView
+from products.views import ProductsListView, ProductDetailView, ProductCommentView
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='list'),
     path('detail/<int:pk>/', ProductDetailView.as_view(), name='detail'),
+    path('comment/<int:pk>/', ProductCommentView.as_view(), name='comment'),
+
 ]
